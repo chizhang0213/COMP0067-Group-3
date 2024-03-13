67587255f4697801e549f172fb07ce4e58a6a2e9
peichipeng <118533573+peichipeng@users.noreply.github.com>
2024-03-13 17:11:18 +0000

    TabDesign section

--- src/sections/moduleCode/TabDesign.js
+++ src/sections/moduleCode/TabDesign.js
@@ -9,19 +9,22 @@ const TabDesign = () => {
   const projects = useGenerateProjects({ moduleNo, academicYearInt });
 
   return (
-    <div>
-      <h2>Projects:</h2>
-      <ul>
-        {projects.map((project, index) => (
-          <li key={index}>
-            <h3>Project {index + 1}</h3>
-            <p>TAId: {project.TAId}</p>
-            <p>Leader: {project.leader}</p>
-            <p>Project Title: {project.projectTitle}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <button>hey</button>
+    </>
+    // <div>
+    //   <h2>Projects:</h2>
+    //   <ul>
+    //     {projects.map((project, index) => (
+    //       <li key={index}>
+    //         <h3>Project {index + 1}</h3>
+    //         <p>TAId: {project.TAId}</p>
+    //         <p>Leader: {project.leader}</p>
+    //         <p>Project Title: {project.projectTitle}</p>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
   );
 };
 
